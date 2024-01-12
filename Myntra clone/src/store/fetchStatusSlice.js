6c9f31e0e5f4d407ccea96529d09f9d819ca495b
Virender Chauhan <bikuchauhan786+github@gmail.com>
2024-01-12 22:58:25 +0530

    created myntra clone using react

---
+++ Myntra clone/src/store/fetchStatusSlice.js
@@ -0,0 +1,23 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+
+const fetchStatusSlice=createSlice({
+  name:"fetchStatus",
+  initialState:{
+    fetchDone:false,
+    currentlyFetching:false,
+  },
+  reducers: {
+    markFetchDone:(state)=>{
+    return void(state.fetchDone = true);
+    },
+    markFetchingStarted:(state)=>{
+      return void(state.currentlyFetching = true);
+    },
+    markFetchingFinished:(state)=>{
+      return void(state.currentlyFetching = false);
+    }
+  }
+})
+export const fetchStatusActions = fetchStatusSlice.actions
+export default fetchStatusSlice;
